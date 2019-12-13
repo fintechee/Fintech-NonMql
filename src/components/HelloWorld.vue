a10d058c9d401c6e2aaadf5b44b6ad4acf3f5924
marksea <3062188557@qq.com>
2019-12-13 17:07:47 +0800

    20191213

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,12 +22,28 @@
             <v-layout justify-center>
                 <v-card class="ma-5" min-width="400">
                     <v-form class="pa-5" ref="form">
-                        <v-text-field label="Broker" placeholder="Broker" v-model="broker" :rules="brokerRules"></v-text-field>
-                        <v-text-field label="Account" placeholder="Account" v-model="account" :rules="accountRules"></v-text-field>
-                        <v-text-field label="Instrument" placeholder="Instrument" v-model="instrument" :rules="instrumentRules"></v-text-field>
-                        <v-text-field label="Indicator" placeholder="Indicator" v-model="indicator" :rules="indicatorRules"></v-text-field>
+                        <v-text-field label="Broker(Optional, Default: Current Broker)" placeholder="Broker" v-model="broker" :rules="brokerRules"></v-text-field>
+                        <v-text-field label="Account(Optional, Default: Current Account)" placeholder="Account" v-model="account" :rules="accountRules"></v-text-field>
+                        <v-select
+                          :items="instrumentNames"
+                          label="Instrument"
+                          v-model="instrument"
+                          :rules="instrumentRules"
+                        ></v-select>
+                        <v-select
+                          :items="timeFrames"
+                          label="Time Frame"
+                          v-model="timeFrame"
+                          :rules="timeFrameRules"
+                        ></v-select>
+                        <v-select
+                          :items="indicatorNames"
+                          label="Indicator"
+                          v-model="indicator"
+                          :rules="indicatorRules"
+                        ></v-select>
 
-                        <v-btn text @click="clearForm()">Clear</v-btn>
+                        <v-btn text @click="clearForm()">Reset</v-btn>
                         <v-btn color="warning" dark @click="generateSourceCode()">Generate</v-btn>
                     </v-form>
                 </v-card>
@@ -51,54 +67,84 @@ export default {
     data: () => ({
             broker: null,
             account: null,
-            indicator: null,
+            instrument: 'EUR/USD',
+            timeFrame: 'TIME_FRAME.M1',
+            indicator: 'sma',
             brokerRules: [
-                v => !!v || 'Broker is required',
-                v => (!!v && v.length <= 20) || 'Too many characters.'
+                v => typeof v == 'undefined' || v == null || v == '' || v.length <= 20 || 'Too many characters.(Limit: 20 characters)'
             ],
             accountRules: [
-                v => !!v || 'Account is required',
-                v => (!!v && v.length <= 20) || 'Too many characters.'
+                v => typeof v == 'undefined' || v == null || v == '' || v.length <= 20 || 'Too many characters.(Limit: 20 characters)'
             ],
             instrumentRules: [
-                v => !!v || 'Instrument is required',
-                v => (!!v && v.length <= 20) || 'Too many characters.'
+                v => v != '' || 'Instrument is required'
+            ],
+            timeFrameRules: [
+                v => v != '' || 'Time Frame is required'
             ],
             indicatorRules: [
-                v => !!v || 'Indicator is required',
-                v => (!!v && v.length <= 20) || 'Too many characters.'
+                v => v != '' || 'Indicator is required'
             ],
 
             sourceCode: '',
 
+            instrumentNames: [{
+                text: 'EUR/USD',
+                value: 'EUR/USD'
+            }, {
+                text: 'GBP/USD',
+                value: 'GBP/USD'
+            }, {
+                text: 'USD/JPY',
+                value: 'USD/JPY'
+            }],
+
+            timeFrames: [{
+                text: 'M1',
+                value: 'TIME_FRAME.M1'
+            }, {
+                text: 'M5',
+                value: 'TIME_FRAME.M5'
+            }, {
+                text: 'M15',
+                value: 'TIME_FRAME.M15'
+            }, {
+                text: 'M30',
+                value: 'TIME_FRAME.M30'
+            }, {
+                text: 'H1',
+                value: 'TIME_FRAME.H1'
+            }, {
+                text: 'H4',
+                value: 'TIME_FRAME.H4'
+            }],
+
+            indicatorNames: [{
+                text: 'Simple Moving Average',
+                value: 'sma'
+            }],
+            indicatorMap: [],
+
             template1:
     'registerEA(\n' +
-    '	"sample_using_sma",\n' +
-    '	"A test EA based on sma",\n' +
-    '	[{ // parameters\n' +
-    '		name: "period",\n' +
-    '		value: 20,\n' +
-    '		required: true,\n' +
-    '		type: PARAMETER_TYPE.INTEGER,\n' +
-    '		range: [1, 100]\n' +
-    '	}],\n' +
-    '	function (context) { // Init()\n' +
-    '		var account = getAccount(context, 0)\n' +
-    '		var brokerName = getBrokerNameOfAccount(account)\n' +
-    '		var accountId = getAccountIdOfAccount(account)\n' +
-    '		var symbolName = "EUR/USD"\n' +
+    '   "sample_using_[indicator]",\n' +
+    '   "A test EA based on [indicator]",\n' +
+        '[parameters]' +
+    '   function (context) { // Init()\n' +
+    '       var account = getAccount(context, 0)\n' +
+    '       var brokerName = [broker]\n' +
+    '       var accountId = [account]\n' +
+    '       var symbolName = [instrument]\n' +
     '       getQuotes(context, brokerName, accountId, symbolName)\n' +
-    '       window.chartHandle = getChartHandle(context, brokerName, accountId, symbolName, TIME_FRAME.M1)\n' +
-    '       var period = getEAParameter(context, "period")\n' +
-    '       window.indiHandle = getIndicatorHandle(context, brokerName, accountId, symbolName, TIME_FRAME.M1, "sma", [{\n' +
-    '           name: "period",\n' +
-    '           value: period\n' +
-    '       }])\n' +
-    '    },\n' +
-    '    function(context) { // Deinit()\n' +
+    '       window.chartHandle = getChartHandle(context, brokerName, accountId, symbolName, [timeFrame])\n' +
+            '[getParameters]' +
+    '       window.indiHandle = getIndicatorHandle(context, brokerName, accountId, symbolName, [timeFrame], "[indicator]",\n' +
+            '[setParameters]' +
+    '   },\n' +
+    '   function(context) { // Deinit()\n' +
     '       delete window.currTime\n' +
-    '    },\n' +
-    '    function(context) { // OnTick()\n' +
+    '   },\n' +
+    '   function(context) { // OnTick()\n' +
     '       var arrTime = getData(context, window.chartHandle, DATA_NAME.TIME)\n' +
     '       if (typeof window.currTime == "undefined") {\n' +
     '           window.currTime = arrTime[arrTime.length - 1]\n' +
@@ -108,35 +154,60 @@ export default {
     '           return\n' +
     '       }\n' +
     '       var account = getAccount(context, 0)\n' +
-    '       var brokerName = getBrokerNameOfAccount(account)\n' +
-    '       var accountId = getAccountIdOfAccount(account)\n' +
-    '       var symbolName = "EUR/USD"\n' +
+    '       var brokerName = [account]\n' +
+    '       var accountId = [account]\n' +
+    '       var symbolName = [instrument]\n' +
     '       var arrClose = getData(context, window.chartHandle, DATA_NAME.CLOSE)\n' +
-    '       var arrSma = getData(context, window.indiHandle, "sma")\n' +
+    '       var arrIndi = getData(context, window.indiHandle, "[indicator]")\n' +
     '       var ask = getAsk(context, brokerName, accountId, symbolName)\n' +
     '       var bid = getBid(context, brokerName, accountId, symbolName)\n' +
     '       var limitPrice = 0.0003\n' +
     '       var stopPrice = 0.0003\n' +
     '       var volume = 0.01\n' +
-    '       if (arrClose[arrClose.length - 3] < arrSma[arrSma.length - 3] && arrClose[arrClose.length - 2] > arrSma[arrSma.length - 2]) {\n' +
+    '       if (arrClose[arrClose.length - 3] < arrIndi[arrIndi.length - 3] && arrClose[arrClose.length - 2] > arrIndi[arrIndi.length - 2]) {\n' +
     '           sendOrder(brokerName, accountId, symbolName, ORDER_TYPE.OP_BUYLIMIT, ask - limitPrice, 0, volume, ask + limitPrice, bid - 3 * stopPrice, "")\n' +
-    '       } else if (arrClose[arrClose.length - 3] > arrSma[arrSma.length - 3] && arrClose[arrClose.length - 2] < arrSma[arrSma.length - 2]) {\n' +
+    '       } else if (arrClose[arrClose.length - 3] > arrIndi[arrIndi.length - 3] && arrClose[arrClose.length - 2] < arrIndi[arrIndi.length - 2]) {\n' +
     '           sendOrder(brokerName, accountId, symbolName, ORDER_TYPE.OP_SELLLIMIT, bid + limitPrice, 0, volume, bid - limitPrice, ask + 3 * stopPrice, "")\n' +
     '       }\n' +
     '    }\n' +
     ')\n'
 }),
 
-components: {
+    components: {
         editor: require('vue2-ace-editor'),
+    },
+    created() {
+        this.init();
     },
 
     methods: {
+        init() {
+            this.indicatorMap['sma'] = {
+                name: 'sma',
+                parameters:
+                '   [{ // parameters\n' +
+                '       name: "period",\n' +
+                '       value: 20,\n' +
+                '       required: true,\n' +
+                '       type: PARAMETER_TYPE.INTEGER,\n' +
+                '       range: [1, 100]\n' +
+                '   }],\n',
+                getParameters:
+                '       var period = getEAParameter(context, "period")\n',
+                setParameters:
+                '       [{\n' +
+                '           name: "period",\n' +
+                '           value: period\n' +
+                '       }])\n',
+            }
+        },
         clearForm() {
             this.broker = null;
             this.account = null;
-            this.indicator = null;
-            this.$refs.form.reset()
+            this.$refs.form.reset();
+            this.instrument = 'EUR/USD';
+            this.timeFrame = 'TIME_FRAME.M1';
+            this.indicator = 'sma';
         },
         editorInit() {
             require('brace/ext/language_tools') //language extension prerequsite...
@@ -145,10 +216,23 @@ components: {
             require('brace/mode/less')
             require('brace/theme/chrome')
             require('brace/snippets/javascript') //snippet
-            this.sourceCode = this.template1;
         },
         generateSourceCode() {
-            this.sourceCode = this.template1;
+            let broker = !this.broker ? 'getBrokerNameOfAccount(account)' : '"' + this.broker + '"';
+            let account = !this.account ? 'getAccountIdOfAccount(account)' : '"' + this.account + '"';
+            let instrument = '"' + this.instrument + '"';
+            let timeFrame = this.timeFrame;
+            let indicator = this.indicatorMap[this.indicator];
+
+            this.sourceCode = this.template1
+                .replace(/\[broker\]/g, broker)
+                .replace(/\[account\]/g, account)
+                .replace(/\[instrument\]/g, instrument)
+                .replace(/\[timeFrame\]/g, timeFrame)
+                .replace(/\[indicator\]/g, indicator.name)
+                .replace(/\[parameters\]/g, indicator.parameters)
+                .replace(/\[getParameters\]/g, indicator.getParameters)
+                .replace(/\[setParameters\]/g, indicator.setParameters);
         }
     },
 };
